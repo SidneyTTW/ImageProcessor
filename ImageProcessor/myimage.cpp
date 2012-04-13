701603307658209b17c3086a8dee026970a01e77
Sidney Wang <sidneyttw@gmail.com>
2012-04-13 15:21:15 +0800

    Fixed the bug found in last commit. It turns out that I shouldn't trust the things I've never tried. The "convertToFormat(QImage::Format_Indexed8)" performs quite poor!
    Signed-off-by: SidneyTTW <sidneyttw@gmail.com>

--- ImageProcessor/myimage.cpp
+++ ImageProcessor/myimage.cpp
@@ -1,5 +1,7 @@
 #include "myimage.h"
 
+#include "imagealgorithm.h"
+
 MyImage::MyImage(const QImage& image, ImageTypeFlag type) :
     _type(type)
 {
@@ -11,16 +13,56 @@ MyImage::MyImage(const QImage& image, ImageTypeFlag type) :
 
 void MyImage::save(const QString& path) const
 {
-  switch (_type)
+  int width = _image.width();
+  int height = _image.height();
+  const unsigned char *imageDataPtr = _image.bits();
+  int realWidth = _image.bytesPerLine();
+  const unsigned char *backup = imageDataPtr;
+
+  bool hasColor = false;
+
+  for(int i = 0;i < height;++i)
   {
-  case MyImage::Colored:
+    imageDataPtr = backup + realWidth * i;
+    for(int j = 0;j < width;++j)
+    {
+      int r, g, b, a;
+      ImageAlgorithm::getRGBA(imageDataPtr, r, g, b, a);
+      if (r != g || r != b)
+      {
+        hasColor = true;
+        break;
+      }
+      imageDataPtr += 4;
+    }
+  }
+
+  if (hasColor)
     _image.save(path);
-    break;
-  case MyImage::Gray:
-  case MyImage::BlackAndWhite:
-    _image.convertToFormat(QImage::Format_Indexed8).save(path);
-    break;
-  default:
-    break;
+  else
+  {
+    QImage grayImage(width, height, QImage::Format_Indexed8);
+    unsigned char *grayDataPtr = grayImage.bits();
+    int realWidth2 = grayImage.bytesPerLine();
+    unsigned char *backup2 = grayDataPtr;
+    QVector<QRgb> table;
+    for (int i = 0;i < MAX_COLOR_VALUE;++i)
+      table.push_back(qRgb(i, i, i));
+    grayImage.setColorTable(table);
+
+    for(int i = 0;i < height;++i)
+    {
+      imageDataPtr = backup + realWidth * i;
+      grayDataPtr = backup2 + realWidth2 * i;
+      for(int j = 0;j < width;++j)
+      {
+        int r, g, b, a;
+        ImageAlgorithm::getRGBA(imageDataPtr, r, g, b, a);
+        *grayDataPtr = (char) r;
+        ++grayDataPtr;
+        imageDataPtr += 4;
+      }
+    }
+    grayImage.save(path);
   }
 }
